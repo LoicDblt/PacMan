487b44ba035064a6c653b495fab906775502fdc0
Loïc DIEBOLT <loic@dblt.fr>
2023-04-03 23:13:25 +0200

    Corrections coordonnées

--- coordinate.h
+++ coordinate.h
@@ -17,34 +17,36 @@ class Coordinate
 		 * l -> "left"
 		 * d -> "down"
 		 * u -> "up"
+		 * 
+		 * Format (x, y, w, h)
 		*/
-		
-		inline static std::vector<SDL_Rect> pac_b{{3, 89, 14, 15}};
-		inline static std::vector<SDL_Rect> pac_r{{20, 89, 14, 15}, {35, 89, 10, 15}};
-		inline static std::vector<SDL_Rect> pac_l{{47, 89, 14, 15}, {63, 89, 10, 15}};
-		inline static std::vector<SDL_Rect> pac_d{{109, 89, 16, 16}, {126, 94, 16, 11}};
-		inline static std::vector<SDL_Rect> pac_u{{75, 89, 16, 16}, {92, 94, 16, 11}};
-		
-		inline static std::vector<SDL_Rect> ghost_red_r{{3, 123, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_red_l{{37, 123, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_red_d{{105, 123, 18, 15}};
-		inline static std::vector<SDL_Rect> ghost_red_u{{71, 123, 15, 15}};
 
-		inline static std::vector<SDL_Rect> ghost_pink_r{{3, 142, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_pink_l{{37, 142, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_pink_d{{105, 142, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_pink_u{{71, 142, 15, 15}};
+		inline static std::vector<SDL_Rect> pac_b{{3, 89, 16, 16}};
+		inline static std::vector<SDL_Rect> pac_r{{20, 89, 15, 16}, {35, 89, 11, 16}};
+		inline static std::vector<SDL_Rect> pac_l{{47, 89, 15, 16}, {63, 89, 11, 16}};
+		inline static std::vector<SDL_Rect> pac_u{{75, 90, 16, 15}, {92, 94, 16, 11}};
+		inline static std::vector<SDL_Rect> pac_d{{109, 90, 16, 15}, {126, 94, 16, 11}};
 
-		inline static std::vector<SDL_Rect> ghost_blue_r{{3, 159, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_blue_l{{37, 159, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_blue_d{{105, 159, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_blue_u{{71, 159, 15, 15}};
+		inline static std::vector<SDL_Rect> ghost_red_r{{3, 123, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_red_l{{37, 123, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_red_u{{71, 124, 16, 15}};
+		inline static std::vector<SDL_Rect> ghost_red_d{{105, 123, 16, 16}};
 
-		inline static std::vector<SDL_Rect> ghost_orange_r{{3, 177, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_orange_l{{37, 177, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_orange_d{{105, 177, 15, 15}};
-		inline static std::vector<SDL_Rect> ghost_orange_u{{71, 177, 15, 15}};
-	
+		inline static std::vector<SDL_Rect> ghost_pink_r{{3, 141, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_pink_l{{37, 141, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_pink_u{{71, 142, 16, 15}};
+		inline static std::vector<SDL_Rect> ghost_pink_d{{105, 141, 16, 16}};
+
+		inline static std::vector<SDL_Rect> ghost_blue_r{{3, 159, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_blue_l{{37, 159, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_blue_u{{71, 160, 16, 15}};
+		inline static std::vector<SDL_Rect> ghost_blue_d{{105, 159, 16, 16}};
+
+		inline static std::vector<SDL_Rect> ghost_orange_r{{3, 177, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_orange_l{{37, 177, 16, 16}};
+		inline static std::vector<SDL_Rect> ghost_orange_u{{71, 178, 16, 15}};
+		inline static std::vector<SDL_Rect> ghost_orange_d{{105, 177, 16, 16}};
+
 	public:
 		Coordinate(/* args */);
 		~Coordinate();
